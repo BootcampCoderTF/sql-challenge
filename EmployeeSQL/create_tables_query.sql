96ef4f54bb1569575522616de84ec8c21cbb9140
BootcampCoderTF <tmfararira@gmail.com>
2023-12-17 19:07:50 +0000

    complete challange files including ERD image, table creation schema and sql querries!

---
+++ EmployeeSQL/create_tables_query.sql
@@ -0,0 +1,46 @@
+-- Format the datestyle for the database to Month/Day/Year
+ALTER DATABASE "Pewlett Hackard (sql-challenge)" SET datestyle TO "ISO, MDY";
+-- Create the tables:
+
+-- Create the Titles table first to avoid error
+CREATE TABLE titles (
+    title_id VARCHAR(50) PRIMARY KEY,
+    title VARCHAR(255)
+);
+
+-- Create the Employees table second to avoid further errors
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title VARCHAR(255) REFERENCES titles(title_id),
+    birth_date DATE,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    sex VARCHAR(1),
+    hire_date DATE
+);
+
+-- Create the Departments table
+CREATE TABLE departments (
+    dept_no VARCHAR(50) PRIMARY KEY,
+    dept_name VARCHAR(255)
+);
+
+-- Create the Salaries table
+CREATE TABLE salaries (
+    emp_no INT PRIMARY KEY REFERENCES employees(emp_no),
+    salary INT
+);
+
+-- Create the Department Employees table
+CREATE TABLE dept_emp (
+    emp_no INT REFERENCES employees(emp_no),
+    dept_no VARCHAR(50) REFERENCES departments(dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Create the Department Managers table
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(50) REFERENCES departments(dept_no),
+    emp_no INT REFERENCES employees(emp_no),
+    PRIMARY KEY (dept_no, emp_no)
+);
